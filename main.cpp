1711b9b7ff11e307537f5e16e1e5fe4ad0e43b4f
JakubK <jakubkuniszewski1@gmail.com>
2019-12-23 17:05:40 +0100

    Simplify asset loading and SDL initialization

--- main.cpp
+++ main.cpp
@@ -120,6 +120,40 @@ void DrawRectangle(SDL_Surface *screen, int x, int y, int l, int k,
     DrawLine(screen, x + 1, i, l - 2, 1, 0, fillColor);
 };
 
+int InitializeSDL(int * rc,SDL_Window ** window, SDL_Renderer ** renderer, SDL_Surface ** screen, SDL_Texture ** scrtex)
+{
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    printf("SDL_Init error: %s\n", SDL_GetError());
+    return 1;
+  }
+  //*rc = SDL_CreateWindowAndRenderer(0, 0, SDL_WINDOW_FULLSCREEN_DESKTOP,
+  //  window, renderer);
+  *rc = SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0,
+    window, renderer);
+
+  if (*rc != 0)
+  {
+    SDL_Quit();
+    printf("SDL_CreateWindowAndRenderer error: %s\n", SDL_GetError());
+    return 1;
+  };
+
+  SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+  SDL_RenderSetLogicalSize(*renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+  SDL_SetRenderDrawColor(*renderer, 0, 0, 0, 255);
+  SDL_SetWindowTitle(*window, "Frogger");
+
+
+  *screen = SDL_CreateRGBSurface(0, SCREEN_WIDTH, SCREEN_HEIGHT, 32,
+    0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000);
+
+  *scrtex = SDL_CreateTexture(*renderer, SDL_PIXELFORMAT_ARGB8888,
+    SDL_TEXTUREACCESS_STREAMING,
+    SCREEN_WIDTH, SCREEN_HEIGHT);
+
+  return 0;
+}
+
 void CloseSDL(SDL_Surface *screen, SDL_Texture *scrtex, SDL_Window *window, SDL_Renderer *renderer)
 {
   SDL_FreeSurface(screen);
@@ -129,10 +163,20 @@ void CloseSDL(SDL_Surface *screen, SDL_Texture *scrtex, SDL_Window *window, SDL_
   SDL_Quit();
 }
 
-// main
+int LoadAsset(SDL_Surface ** asset, char * path)
+{
+  *asset = SDL_LoadBMP(path);
+  if (*asset == NULL) {
+    printf("SDL_LoadBMP(%s) error: %s\n",path, SDL_GetError());
+    return 1;
+  };
+  return 0;
+}
+
 #ifdef __cplusplus
 extern "C"
 #endif
+
 int main(int argc, char **argv) {
   int t1, t2, quit, frames, rc;
   double delta, worldTime, fpsTimer, fps, distance, etiSpeed;
@@ -140,155 +184,40 @@ int main(int argc, char **argv) {
   SDL_Surface *screen, *charset;
   SDL_Surface *eti;
   SDL_Surface *river, *road, *brick1, *frogger, *froggo;
+  SDL_Surface *turtle;
   SDL_Surface *froggger;
-  SDL_Surface *car;
+  SDL_Surface *log;
+  SDL_Surface *space_car, *rocket_car;
   SDL_Texture *scrtex;
   SDL_Window *window;
   SDL_Renderer *renderer;
 
-  // okno konsoli nie jest widoczne, je¿eli chcemy zobaczyæ
-  // komunikaty wypisywane printf-em trzeba w opcjach:
-  // project -> szablon2 properties -> Linker -> System -> Subsystem
-  // zmieniæ na "Console"
-  // console window is not visible, to see the printf output
-  // the option:
-  // project -> szablon2 properties -> Linker -> System -> Subsystem
-  // must be changed to "Console"
-  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-    printf("SDL_Init error: %s\n", SDL_GetError());
+  int initUnsuccessful = InitializeSDL(&rc, &window, &renderer, &screen, &scrtex);
+  if (initUnsuccessful)
     return 1;
-  }
-  // tryb pe³noekranowy / fullscreen mode
-  //rc = SDL_CreateWindowAndRenderer(0, 0, SDL_WINDOW_FULLSCREEN_DESKTOP,
-  //  &window, &renderer);
-  	rc = SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0,
-  	                                 &window, &renderer);
-  if (rc != 0)
-  {
-    SDL_Quit();
-    printf("SDL_CreateWindowAndRenderer error: %s\n", SDL_GetError());
-    return 1;
-  };
-
-  SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
-  SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-
-  SDL_SetWindowTitle(window, "Frogger");
-
-
-  screen = SDL_CreateRGBSurface(0, SCREEN_WIDTH, SCREEN_HEIGHT, 32,
-    0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000);
-
-  scrtex = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
-    SDL_TEXTUREACCESS_STREAMING,
-    SCREEN_WIDTH, SCREEN_HEIGHT);
-
-  MovingEntity vehicles[5];
-  for (int i = 0; i < 5; i++)
-  {
-    vehicles[i].direction = (i % 2) == 0 ? 1 : -1;
-    vehicles[i].velocity = 80 * (i + 1);
-    vehicles[i].entity.Y = (7 + i) * CELL_SIZE + CELL_SIZE / 2;
-    vehicles[i].entity.X = SCREEN_WIDTH / 2;
-
-    vehicles[i].entity.Width = CELL_SIZE;
-    vehicles[i].entity.Height = CELL_SIZE;
-  }
-
-  MovingEntity logs[3][LOGS_IN_ROW];
-  for (int i = 0; i < 3; i++)
-  {
-    for (int j = 0; j < LOGS_IN_ROW; j++)
-    {
-      logs[i][j].direction = -1;
-      logs[i][j].velocity = 100 * (i+1);
-      logs[i][j].entity.Y = i == 0 ? CELL_SIZE + CELL_SIZE / 2 : (2 + i) * CELL_SIZE + CELL_SIZE / 2;
-      logs[i][j].entity.X = (j * 5) * CELL_SIZE + SCREEN_WIDTH / 2;
-
-      logs[i][j].entity.Width = CELL_SIZE;
-      logs[i][j].entity.Height = CELL_SIZE;
-    }   
-  }
-
-  MovingEntity turtles[2][3];
-  for (int i = 0; i < 2; i++)
-  {
-    for (int j = 0; j < TURTLES_IN_ROW; j++)
-    {
-      turtles[i][j].direction = 1;
-      turtles[i][j].velocity = 110;
-      turtles[i][j].entity.Y = i == 0 ? 2 * CELL_SIZE + CELL_SIZE / 2 : 5 * CELL_SIZE + CELL_SIZE / 2;
-      turtles[i][j].entity.X = (j * 8) * CELL_SIZE + SCREEN_WIDTH / 2;
-
-      turtles[i][j].entity.Width = CELL_SIZE;
-      turtles[i][j].entity.Height = CELL_SIZE;
-    }
-  }
-
-  Endpoint endpoints[5];
-  for (int i = 0; i < 5; i++)
-  {
-    endpoints[i].activated = 0;
-    endpoints[i].entity.Y = CELL_SIZE/2;
-    endpoints[i].entity.X = SCREEN_WIDTH/5/2 + i * SCREEN_WIDTH / 5;
-    endpoints[i].entity.Width = endpoints[i].entity.Height = CELL_SIZE;
-  }
-
-  Entity player;
-  player.X = 7 * CELL_SIZE + CELL_SIZE / 2;
-  player.Y = 12 * CELL_SIZE + CELL_SIZE / 2;
-  player.Width = player.Height = CELL_SIZE;
-
-
+   
   // wy³¹czenie widocznoœci kursora myszy
   //SDL_ShowCursor(SDL_DISABLE);
 
-  // wczytanie obrazka cs8x8.bmp
-  charset = SDL_LoadBMP("cs8x8.bmp");
-  if (charset == NULL) {
-    printf("SDL_LoadBMP(cs8x8.bmp) error: %s\n", SDL_GetError());
-    CloseSDL(screen, scrtex, window, renderer);
-    return 1;
-  };
-  SDL_SetColorKey(charset, true, 0x000000);
+  int loadingAssetsUnsuccessful = 0;
+  loadingAssetsUnsuccessful = LoadAsset(&charset, "cs8x8.bmp");
+  loadingAssetsUnsuccessful = LoadAsset(&froggo, "froggie.bmp");
+  loadingAssetsUnsuccessful = LoadAsset(&brick1, "brick1.bmp");
+  loadingAssetsUnsuccessful = LoadAsset(&frogger, "frogger.bmp");
+  loadingAssetsUnsuccessful = LoadAsset(&space_car, "space_car.bmp");
+  loadingAssetsUnsuccessful = LoadAsset(&rocket_car, "rocket_car.bmp");
+  loadingAssetsUnsuccessful = LoadAsset(&log, "log_medium.bmp");
+  loadingAssetsUnsuccessful = LoadAsset(&turtle, "double_turtles.bmp");
+  loadingAssetsUnsuccessful = LoadAsset(&road, "road.bmp");
+  loadingAssetsUnsuccessful = LoadAsset(&froggo, "froggie.bmp");
 
-  eti = SDL_LoadBMP("eti.bmp");
-  if (eti == NULL) {
-    printf("SDL_LoadBMP(eti.bmp) error: %s\n", SDL_GetError());
+  if (loadingAssetsUnsuccessful)
+  {
     CloseSDL(screen, scrtex, window, renderer);
     return 1;
-  };
-  froggo = SDL_LoadBMP("froggie.bmp");
-  if (froggo == NULL) {
-    printf("SDL_LoadBMP(froggie.bmp) error: %s\n", SDL_GetError());
-    CloseSDL(screen, scrtex, window, renderer);
-    return 1;
-  };
-  road = SDL_LoadBMP("road.bmp");
-  if (road == NULL) {
-    printf("SDL_LoadBMP(river.bmp) error: %s\n", SDL_GetError());
-    CloseSDL(screen, scrtex, window, renderer);
-    return 1;
-  };
-  brick1 = SDL_LoadBMP("brick1.bmp");
-  if (brick1 == NULL) {
-    printf("SDL_LoadBMP(brick1.bmp) error: %s\n", SDL_GetError());
-    CloseSDL(screen, scrtex, window, renderer);
-    return 1;
-  };
-  frogger = SDL_LoadBMP("frogger.bmp");
-  if (frogger == NULL) {
-    printf("SDL_LoadBMP(frogger.bmp) error: %s\n", SDL_GetError());
-    CloseSDL(screen, scrtex, window, renderer);
-    return 1;
-  };
-  car = SDL_LoadBMP("car1.bmp");
-  if (car == NULL) {
-    printf("SDL_LoadBMP(car.bmp) error: %s\n", SDL_GetError());
-    CloseSDL(screen, scrtex, window, renderer);
-    return 1;
-  };
+  }
+
+  SDL_SetColorKey(charset, true, 0x000000);
 
   char text[128];
   int czarny = SDL_MapRGB(screen->format, 0x00, 0x00, 0x00);
@@ -308,15 +237,65 @@ int main(int argc, char **argv) {
   distance = 0;
   etiSpeed = 1;
 
+  MovingEntity vehicles[5];
+  for (int i = 0; i < 5; i++)
+  {
+    vehicles[i].direction = (i % 2) == 0 ? 1 : -1;
+    vehicles[i].velocity = 80 * (i + 1);
+    vehicles[i].entity.Y = (7 + i) * CELL_SIZE + CELL_SIZE / 2;
+    vehicles[i].entity.X = SCREEN_WIDTH / 2;
+
+    vehicles[i].entity.Width = rocket_car->w;
+    vehicles[i].entity.Height = CELL_SIZE;
+  }
+
+  MovingEntity logs[3][LOGS_IN_ROW];
+  for (int i = 0; i < 3; i++)
+  {
+    for (int j = 0; j < LOGS_IN_ROW; j++)
+    {
+      logs[i][j].direction = -1;
+      logs[i][j].velocity = 100 * (i + 1);
+      logs[i][j].entity.Y = i == 0 ? CELL_SIZE + CELL_SIZE / 2 : (2 + i) * CELL_SIZE + CELL_SIZE / 2;
+      logs[i][j].entity.X = (j * 5) * CELL_SIZE + SCREEN_WIDTH / 2;
+
+      logs[i][j].entity.Width = log->w;
+      logs[i][j].entity.Height = CELL_SIZE;
+    }
+  }
+
+  MovingEntity turtles[2][3];
+  for (int i = 0; i < 2; i++)
+  {
+    for (int j = 0; j < TURTLES_IN_ROW; j++)
+    {
+      turtles[i][j].direction = 1;
+      turtles[i][j].velocity = 110;
+      turtles[i][j].entity.Y = i == 0 ? 2 * CELL_SIZE + CELL_SIZE / 2 : 5 * CELL_SIZE + CELL_SIZE / 2;
+      turtles[i][j].entity.X = (j * 8) * CELL_SIZE + SCREEN_WIDTH / 2;
+
+      turtles[i][j].entity.Width = 82;
+      turtles[i][j].entity.Height = CELL_SIZE;
+    }
+  }
+
+  Endpoint endpoints[5];
+  for (int i = 0; i < 5; i++)
+  {
+    endpoints[i].activated = 0;
+    endpoints[i].entity.Y = CELL_SIZE / 2;
+    endpoints[i].entity.X = SCREEN_WIDTH / 5 / 2 + i * SCREEN_WIDTH / 5;
+    endpoints[i].entity.Width = endpoints[i].entity.Height = CELL_SIZE;
+  }
+
+  Entity player;
+  player.X = 7 * CELL_SIZE + CELL_SIZE / 2;
+  player.Y = 12 * CELL_SIZE + CELL_SIZE / 2;
+  player.Width = player.Height = CELL_SIZE;
+
   while (!quit) {
     t2 = SDL_GetTicks();
 
-    // w tym momencie t2-t1 to czas w milisekundach,
-    // jaki uplyna³ od ostatniego narysowania ekranu
-    // delta to ten sam czas w sekundach
-    // here t2-t1 is the time in milliseconds since
-    // the last screen was drawn
-    // delta is the same time in seconds
     delta = (t2 - t1) * 0.001;
     t1 = t2;
 
@@ -364,7 +343,6 @@ int main(int argc, char **argv) {
         if (logs[i][j].entity.X < -SCREEN_WIDTH)
           logs[i][j].entity.X = SCREEN_WIDTH;
       }
-      
     }
 
     for (int i = 0; i < 2; i++)
@@ -374,7 +352,7 @@ int main(int argc, char **argv) {
           turtles[i][j].entity.X = 0;
     }
 
-    //Check if frog intersects with the car
+    //Check if frog intersects with the space_car
     for (int i = 0; i < 5; i++)
     {
       if (Intersects(player, vehicles[i].entity))
@@ -430,10 +408,7 @@ int main(int argc, char **argv) {
       if (!intersects)
         quit = 1;
     }
-    else //try to clamp the position X
-    {
-      
-    }
+
     SDL_FillRect(screen, NULL, niebieski);
 
     //Check if Frog is out of Screen
@@ -462,7 +437,7 @@ int main(int argc, char **argv) {
     {
       for (int j = 0; j < LOGS_IN_ROW; j++)
       {
-        DrawSurface(screen, car,
+        DrawSurface(screen, log,
           logs[i][j].entity.X,
           logs[i][j].entity.Y);
       } 
@@ -472,7 +447,7 @@ int main(int argc, char **argv) {
     {
       for (int j = 0; j < TURTLES_IN_ROW; j++)
       {
-        DrawSurface(screen, car,
+        DrawSurface(screen, turtle,
           turtles[i][j].entity.X,
           turtles[i][j].entity.Y);
       }
@@ -484,7 +459,7 @@ int main(int argc, char **argv) {
 
     for (int i = 0; i < 5; i++)
     {
-      DrawSurface(screen, car,
+      DrawSurface(screen, vehicles[i].direction == 1 ? rocket_car : space_car,
         vehicles[i].entity.X,
         vehicles[i].entity.Y);
     }
@@ -533,25 +508,20 @@ int main(int argc, char **argv) {
         if (event.key.keysym.sym == SDLK_ESCAPE || event.key.keysym.sym == SDLK_q)
           quit = 1;
         else if (event.key.keysym.sym == SDLK_UP)
-        {
           if(player.Y - CELL_SIZE >= 0)
             player.Y -= CELL_SIZE;
-        }
         else if (event.key.keysym.sym == SDLK_DOWN)
-        {
           if(player.Y + CELL_SIZE <= CELL_SIZE * 13)
             player.Y += CELL_SIZE;
-        }
         else if (event.key.keysym.sym == SDLK_LEFT)
-        {
           if(player.X - CELL_SIZE >= 0)
             player.X -= CELL_SIZE;
-        }
         else if (event.key.keysym.sym == SDLK_RIGHT)
-        {
           if(player.X + CELL_SIZE <= CELL_SIZE * 14)
             player.X += CELL_SIZE;
-        }
+        break;
+      case SDL_QUIT:
+        quit = 1;
         break;
       };
     };
